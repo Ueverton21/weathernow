54099fe16cf4d37b416ec01c9d57d6a835139dd1
Ueverton21 <uevertonandrade@gmail.com>
2020-04-04 13:54:29 -0300

    Iniciando projeto

---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>HOME DA PÁGINA</h2>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Home',
+    created(){
+        console.log("HOME");
+    }
+}
+
+</script>
+
+<style scoped>
+
+h1{
+    color: rgb(19, 148, 199);
+}
+
+</style>
---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/pages/Home';
+import Detalhes from '@/components/Detalhes';
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home,
+        },
+        {
+            path: '/details',
+            name: 'Detalhes',
+            component: Detalhes,
+        },
+    ]
+});
